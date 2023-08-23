c7e7163005076b4059c9760edb489cfb30aed7b2
Michael <michaellngriffin@gmail.com>
2023-08-23 18:24:13 -0400

    madlibs working

--- flask-madlibs/app.py
+++ flask-madlibs/app.py
@@ -1,14 +1,35 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import silly_story, excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+
+@app.get('/home')
+def show_home():
+    #display dropdown menu that selects between silly_story +
+    # excited_story
+    madlib_templates = [silly_story, excited_story]
+
+    return render_template('home.html',
+                           story_templates = madlib_templates)
+
 @app.get('/questions')
 def load_questions():
+    """"load the prompt for madlibs words"""
+    return render_template('questions.html',
+                           words=silly_story.prompts)
 
-    return render_template('questions.html', words=silly_story.prompts)
+@app.get('/results')
+def show_story():
+    """After getting user's answers to prompt, display story"""
+    #grab data from form submission
+    answers = request.args
+    #set answers in render_template to result of get_result_text()
+    full_story = silly_story.get_result_text(answers)
+    return render_template('results.html',
+                           story = full_story)
