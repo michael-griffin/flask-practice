46678c8662cc80d3643c723c6544664852d7849e
Daniel Zych <danjzych@gmail.com>
2023-08-23 17:58:24 -0500

    Started on Flask Madlibs, implemented questions page

---
+++ flask-madlibs/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+
+from stories import silly_story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+
+@app.get('/questions')
+def load_questions():
+
+    return render_template('questions.html', words=silly_story.prompts)
