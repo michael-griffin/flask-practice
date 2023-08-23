bdf3b73567a2268f918a034dcee71002321fd378
Michael <michaellngriffin@gmail.com>
2023-08-23 14:33:05 -0400

    greet app working

---
+++ greet/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# /welcome
+#     Returns “welcome”
+# /welcome/home
+#     Returns “welcome home”
+# /welcome/back
+#     Return “welcome back”
+@app.get('/welcome')
+def show_welcome():
+    return "<h1>welcome</h1>"
+
+@app.get('/home')
+def show_home():
+    return "<h1>welcome home</h1>"
+
+@app.get('/back')
+def show_back():
+    return "<h1>welcome back</h1>"
