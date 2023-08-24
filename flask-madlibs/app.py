892cf3ee76f5bc9b35147f24059be81f6f5e60b4
Daniel Zych <danjzych@gmail.com>
2023-08-23 19:04:03 -0500

    completed further study

--- flask-madlibs/app.py
+++ flask-madlibs/app.py
@@ -8,12 +8,16 @@ app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+TEMPLATES = {
+    'silly': silly_story,
+    'excited': excited_story
+}
 
 @app.get('/home')
 def show_home():
     #display dropdown menu that selects between silly_story +
     # excited_story
-    madlib_templates = [silly_story, excited_story]
+    madlib_templates = ['silly', 'excited']
 
     return render_template('home.html',
                            story_templates = madlib_templates)
@@ -21,15 +25,21 @@ def show_home():
 @app.get('/questions')
 def load_questions():
     """"load the prompt for madlibs words"""
+    choice = request.args['story_template']
+
     return render_template('questions.html',
-                           words=silly_story.prompts)
+                           words=TEMPLATES[choice].prompts,
+                           story_template=choice)
 
 @app.get('/results')
 def show_story():
     """After getting user's answers to prompt, display story"""
-    #grab data from form submission
-    answers = request.args
-    #set answers in render_template to result of get_result_text()
-    full_story = silly_story.get_result_text(answers)
+    answers = {}
+    for (key, val) in request.args.items():
+        if key != 'story_template':
+            answers[key] = val
+
+    choice = request.args['story_template']
+    full_story = TEMPLATES[choice].get_result_text(answers)
     return render_template('results.html',
                            story = full_story)
